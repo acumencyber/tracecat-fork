eaf060ff06ffa83ac9e420acec8610fee0790bfa
benacumen <159263176+benacumen@users.noreply.github.com>
2024-08-17 13:45:19 +0100

    Add acumen integrations via Upload
    Signed-off-by: benacumen <159263176+benacumen@users.noreply.github.com>

--- tracecat/actions/integrations/siem/__init__.py
+++ tracecat/actions/integrations/siem/__init__.py
@@ -1,7 +1,8 @@
 from .datadog import list_datadog_alerts
 from .elastic import list_elastic_alerts
-
+from .acumen_elastic import acumen_list_elastic_alerts
 __all__ = [
     "list_datadog_alerts",
     "list_elastic_alerts",
+    "acumen_list_elastic_alerts"
 ]
